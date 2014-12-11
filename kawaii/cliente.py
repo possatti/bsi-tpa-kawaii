79fa5f9bacf57d7cd16a9913e590ff4260fdf3c7
Lucas Possatti <lucas_possatti@hotmail.com>
2014-12-11 12:03:57 -0200

    Criação de métodos de comparação entre clientes

--- kawaii/cliente.py
+++ kawaii/cliente.py
@@ -1,7 +1,10 @@
+from functools import total_ordering
 
+@total_ordering
 class Cliente:
 
 	def __init__(self, nome, sobrenome, endereco, telefone, saldo):
+		# TODO: self.numero
 		self.nome = nome
 		self.sobrenome = sobrenome
 		self.endereco = endereco
@@ -15,3 +18,29 @@ class Cliente:
 			"endereco: " + self.endereco + ", " \
 			"telefone: " + self.telefone + ", " \
 			"saldo: " + str(self.saldo) + "]"
+
+	def __eq__(self, other):
+		return (
+			(self.nome.lower(),
+			self.sobrenome.lower(),
+			self.endereco.lower(),
+			self.telefone.lower(),
+			self.saldo) ==
+				(other.nome.lower(),
+				other.sobrenome.lower(),
+				other.endereco.lower(),
+				other.telefone.lower(),
+				other.saldo))
+
+	def __lt__(self, other):
+		return (
+			(self.saldo,
+			self.nome.lower(),
+			self.sobrenome.lower(),
+			self.endereco.lower(),
+			self.telefone.lower()) <
+				(other.saldo,
+				other.nome.lower(),
+				other.sobrenome.lower(),
+				other.endereco.lower(),
+				other.telefone.lower()))
