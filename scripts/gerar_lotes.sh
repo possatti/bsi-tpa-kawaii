6d8c926ece5209a5712e52f20ce7509039bd0f5d
Lucas Possatti <lucas_possatti@hotmail.com>
2014-12-17 00:22:38 -0200

    Criação de um script facilitador para geração de arquivos

---
+++ scripts/gerar_lotes.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Série de comandos para gerar lotes de clientes desordenados
+kwii gerar 50000
+kwii gerar 100000
+kwii gerar 150000
+kwii gerar 200000
+kwii gerar 250000
+kwii gerar 300000
+kwii gerar 350000
+kwii gerar 400000
+kwii gerar 450000
+kwii gerar 500000
